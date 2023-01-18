2b22fd702dfb2eb0039f00b0dd19e873fc636025
Brett Stephen <igadget777@gmail.com>
2023-01-18 15:34:54 -0500

    Removed out webserver

--- main.tf
+++ main.tf
@@ -2,115 +2,115 @@
 // Licensed under the Mozilla Public License v2.0
 
 
-# variable "compartment_ocid" {}
-# variable "region" {}
-# variable "tenancy_ocid" {}
-# variable "user_ocid" {}
-# variable "fingerprint" {}
-# variable "private_key" {}
-# variable "ssh_public_key" {}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key" {}
+variable "ssh_public_key" {}
 
-# provider "oci" {
-#   tenancy_ocid     = var.tenancy_ocid
-#   user_ocid        = var.user_ocid
-#   fingerprint      = var.fingerprint
-#   private_key      = var.private_key
-#   region           = var.region
-# }
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key      = var.private_key
+  region           = var.region
+}
 
-# variable "ad_region_mapping" {
-#   type = map(string)
+variable "ad_region_mapping" {
+  type = map(string)
 
-#   default = {
-#     # us-phoenix-1  = 2
-#     us-ashburn-1 = 2
-#     # sa-saopaulo-1 = 1
-#   }
-# }
+  default = {
+    # us-phoenix-1  = 2
+    us-ashburn-1 = 2
+    # sa-saopaulo-1 = 1
+  }
+}
 
-# variable "images" {
-#   type = map(string)
+variable "images" {
+  type = map(string)
 
-#   default = {
-#     # See https://docs.us-phoenix-1.oraclecloud.com/images/
-#     # Oracle-provided image "Oracle-Linux-7.9-2020.10.26-0"
-#     # us-phoenix-1  = "ocid1.image.oc1.phx.aaaaaaaacirjuulpw2vbdiogz3jtcw3cdd3u5iuangemxq5f5ajfox3aplxa"
-#     us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaabbg2rypwy5pwnzinrutzjbrs3r35vqzwhfjui7yibmydzl7qgn6a"
-#     # sa-saopaulo-1 = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaaudio63gdicxwujhfok7jdyewf6iwl6sgcaqlyk4fvttg3bw6gbpq"
-#   }
-# }
+  default = {
+    # See https://docs.us-phoenix-1.oraclecloud.com/images/
+    # Oracle-provided image "Oracle-Linux-7.9-2020.10.26-0"
+    # us-phoenix-1  = "ocid1.image.oc1.phx.aaaaaaaacirjuulpw2vbdiogz3jtcw3cdd3u5iuangemxq5f5ajfox3aplxa"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaabbg2rypwy5pwnzinrutzjbrs3r35vqzwhfjui7yibmydzl7qgn6a"
+    # sa-saopaulo-1 = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaaudio63gdicxwujhfok7jdyewf6iwl6sgcaqlyk4fvttg3bw6gbpq"
+  }
+}
 
-# data "oci_identity_availability_domain" "ad" {
-#   compartment_id = var.tenancy_ocid
-#   ad_number      = var.ad_region_mapping[var.region]
-# }
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = var.ad_region_mapping[var.region]
+}
 
-# resource "oci_core_virtual_network" "tcb_vcn" {
-#   cidr_block     = "10.1.0.0/16"
-#   compartment_id = var.compartment_ocid
-#   display_name   = "tcbVCN"
-#   dns_label      = "tcbvcn"
-# }
+resource "oci_core_virtual_network" "tcb_vcn" {
+  cidr_block     = "10.1.0.0/16"
+  compartment_id = var.compartment_ocid
+  display_name   = "tcbVCN"
+  dns_label      = "tcbvcn"
+}
 
-# resource "oci_core_subnet" "tcb_subnet" {
-#   cidr_block        = "10.1.20.0/24"
-#   display_name      = "tcbSubnet"
-#   dns_label         = "tcbsubnet"
-#   security_list_ids = [oci_core_security_list.tcb_security_list.id]
-#   compartment_id    = var.compartment_ocid
-#   vcn_id            = oci_core_virtual_network.tcb_vcn.id
-#   route_table_id    = oci_core_route_table.tcb_route_table.id
-#   dhcp_options_id   = oci_core_virtual_network.tcb_vcn.default_dhcp_options_id
-# }
+resource "oci_core_subnet" "tcb_subnet" {
+  cidr_block        = "10.1.20.0/24"
+  display_name      = "tcbSubnet"
+  dns_label         = "tcbsubnet"
+  security_list_ids = [oci_core_security_list.tcb_security_list.id]
+  compartment_id    = var.compartment_ocid
+  vcn_id            = oci_core_virtual_network.tcb_vcn.id
+  route_table_id    = oci_core_route_table.tcb_route_table.id
+  dhcp_options_id   = oci_core_virtual_network.tcb_vcn.default_dhcp_options_id
+}
 
-# resource "oci_core_internet_gateway" "tcb_internet_gateway" {
-#   compartment_id = var.compartment_ocid
-#   display_name   = "tcbIG"
-#   vcn_id         = oci_core_virtual_network.tcb_vcn.id
-# }
+resource "oci_core_internet_gateway" "tcb_internet_gateway" {
+  compartment_id = var.compartment_ocid
+  display_name   = "tcbIG"
+  vcn_id         = oci_core_virtual_network.tcb_vcn.id
+}
 
-# resource "oci_core_route_table" "tcb_route_table" {
-#   compartment_id = var.compartment_ocid
-#   vcn_id         = oci_core_virtual_network.tcb_vcn.id
-#   display_name   = "tcbRouteTable"
+resource "oci_core_route_table" "tcb_route_table" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.tcb_vcn.id
+  display_name   = "tcbRouteTable"
 
-#   route_rules {
-#     destination       = "0.0.0.0/0"
-#     destination_type  = "CIDR_BLOCK"
-#     network_entity_id = oci_core_internet_gateway.tcb_internet_gateway.id
-#   }
-# }
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.tcb_internet_gateway.id
+  }
+}
 
-# resource "oci_core_security_list" "tcb_security_list" {
-#   compartment_id = var.compartment_ocid
-#   vcn_id         = oci_core_virtual_network.tcb_vcn.id
-#   display_name   = "tcbSecurityList"
+resource "oci_core_security_list" "tcb_security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.tcb_vcn.id
+  display_name   = "tcbSecurityList"
 
-#   egress_security_rules {
-#     protocol    = "6"
-#     destination = "0.0.0.0/0"
-#   }
+  egress_security_rules {
+    protocol    = "6"
+    destination = "0.0.0.0/0"
+  }
 
-#   ingress_security_rules {
-#     protocol = "6"
-#     source   = "0.0.0.0/0"
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
 
-#     tcp_options {
-#       max = "22"
-#       min = "22"
-#     }
-#   }
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
 
-#   ingress_security_rules {
-#     protocol = "6"
-#     source   = "0.0.0.0/0"
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
 
-#     tcp_options {
-#       max = "80"
-#       min = "80"
-#     }
-#   }
-# }
+    tcp_options {
+      max = "80"
+      min = "80"
+    }
+  }
+}
 
 # resource "oci_core_instance" "webserver1" {
 #   availability_domain = data.oci_identity_availability_domain.ad.name
