78e192ee19197da896912743184fe360010c6d0a
Brett Stephen <igadget777@gmail.com>
2023-01-19 12:00:13 -0500

    Added webserver 1

--- main.tf
+++ main.tf
@@ -112,25 +112,25 @@ resource "oci_core_security_list" "tcb_security_list" {
   }
 }
 
-# resource "oci_core_instance" "webserver1" {
-#   availability_domain = data.oci_identity_availability_domain.ad.name
-#   compartment_id      = var.compartment_ocid
-#   display_name        = "webserver1"
-#   shape               = "VM.Standard.E2.1.Micro"
+resource "oci_core_instance" "webserver1" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.compartment_ocid
+  display_name        = "webserver1"
+  shape               = "VM.Standard.E2.1.Micro"
 
-#   create_vnic_details {
-#     subnet_id        = oci_core_subnet.tcb_subnet.id
-#     display_name     = "primaryvnic"
-#     assign_public_ip = true
-#     hostname_label   = "webserver1"
-#   }
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.tcb_subnet.id
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "webserver1"
+  }
 
-#   source_details {
-#     source_type = "image"
-#     source_id   = var.images[var.region]
-#   }
+  source_details {
+    source_type = "image"
+    source_id   = var.images[var.region]
+  }
 
-#   metadata = {
-#     ssh_authorized_keys = var.ssh_public_key
-#   }
-# }
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
+}
