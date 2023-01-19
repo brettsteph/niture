c3c5ab9933bab8d64f1d5b3dc85e1af8171d7492
Brett Stephen <igadget777@gmail.com>
2023-01-19 12:42:35 -0500

    Removed web server 2

--- main.tf
+++ main.tf
@@ -113,17 +113,16 @@ resource "oci_core_security_list" "tcb_security_list" {
 }
 
 resource "oci_core_instance" "webservers" {
-  count               = 2
   availability_domain = data.oci_identity_availability_domain.ad.name
   compartment_id      = var.compartment_ocid
-  display_name        = "webserver${count.index + 1}"
+  display_name        = "webserver1"
   shape               = "VM.Standard.E2.1.Micro"
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.tcb_subnet.id
     display_name     = "primaryvnic"
     assign_public_ip = true
-    hostname_label   = "webserver${count.index + 1}"
+    hostname_label   = "webserver1"
   }
 
   source_details {
